ba134c36cdce49f646683df813a370088bb8ffe3
Nikita Shulga <nshulga@meta.com>
2025-01-13 11:19:32 -0800

    [CD] Enable python3.13t builds for aarch64
    But make sure that right numpy version is picked (2.0.2 does not support 3.13)
    
    ghstack-source-id: a196dd3148d57da8fbbb214d8e453c0c3b5204ba
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/144698

--- .ci/aarch64_linux/aarch64_ci_setup.sh
+++ .ci/aarch64_linux/aarch64_ci_setup.sh
@@ -6,9 +6,14 @@ set -eux -o pipefail
 
 NUMPY_VERSION=2.0.2
 PYGIT2_VERSION=1.15.1
-if [[ "$DESIRED_PYTHON"  == "3.13" ]]; then
+if [[ "$DESIRED_PYTHON"  == "3.13" || "$DESIRED_PYTHON" == "3.13t" ]]; then
     NUMPY_VERSION=2.1.2
     PYGIT2_VERSION=1.16.0
+fi
+
+if [[ "$DESIRED_PYTHON" == "3.13t" ]]; then
+    #PYGIT2 has not been released to 3.13t and needs libgit2 to be built locally
+    sudo yum install libgit2-devel
 fi
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
